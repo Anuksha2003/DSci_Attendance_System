5a56fc7efdde53ecf8b5eb5a0a71bf543863e303
Payal1611 <99703148+Payal1611@users.noreply.github.com>
2024-03-22 22:17:30 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,44 @@
+# import cv2
+# import face_recognition
+
+
+# img = cv2.imread("virat.jpeg")
+# rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+# img_encoding = face_recognition.face_encodings(rgb_img)[0]
+
+# img2 = cv2.imread("images/virat-kohli.jpeg")
+# rgb_img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
+# img_encoding2 = face_recognition.face_encodings(rgb_img2)[0]
+
+# img3 = cv2.imread("images/abd.jpeg")
+# rgb_img3 = cv2.cvtColor(img3, cv2.COLOR_BGR2RGB)
+# img_encoding3 = face_recognition.face_encodings(rgb_img3)[0]
+
+# img4 = cv2.imread("images/dhoni.jpeg")
+# rgb_img4 = cv2.cvtColor(img4, cv2.COLOR_BGR2RGB)
+# img_encoding4 = face_recognition.face_encodings(rgb_img4)[0]
+
+# img5 = cv2.imread("images/rohit.jpeg")
+# rgb_img5 = cv2.cvtColor(img5, cv2.COLOR_BGR2RGB)
+# img_encoding5 = face_recognition.face_encodings(rgb_img5)[0]
+
+
+# images_encodings = [img_encoding2, img_encoding3, img_encoding4, img_encoding5]
+# images_names = ["Virat Kohli", "AB de Villiers", "MS Dhoni", "Rohit Sharma"]
+
+# for i, encoding in enumerate(images_encodings):
+#     result = face_recognition.compare_faces([img_encoding], encoding)
+#     if result[0]:
+#         print("Name:", images_names[i])
+
+
 import cv2
 import face_recognition
 
+group_img = cv2.imread("virat_rohit.jpeg")
+rgb_group_img = cv2.cvtColor(group_img, cv2.COLOR_BGR2RGB)
+group_encodings = face_recognition.face_encodings(rgb_group_img)
 
-img = cv2.imread("virat.jpeg")
-rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-img_encoding = face_recognition.face_encodings(rgb_img)[0]
 
 img2 = cv2.imread("images/virat-kohli.jpeg")
 rgb_img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
@@ -22,11 +56,12 @@ img5 = cv2.imread("images/rohit.jpeg")
 rgb_img5 = cv2.cvtColor(img5, cv2.COLOR_BGR2RGB)
 img_encoding5 = face_recognition.face_encodings(rgb_img5)[0]
 
-
 images_encodings = [img_encoding2, img_encoding3, img_encoding4, img_encoding5]
 images_names = ["Virat Kohli", "AB de Villiers", "MS Dhoni", "Rohit Sharma"]
 
-for i, encoding in enumerate(images_encodings):
-    result = face_recognition.compare_faces([img_encoding], encoding)
-    if result[0]:
-        print("Name:", images_names[i])
+for group_encoding in group_encodings:
+    for i, encoding in enumerate(images_encodings):
+        result = face_recognition.compare_faces([group_encoding], encoding)
+        if result[0]:
+            print("Name:", images_names[i])
+            break  
